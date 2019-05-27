ade9833e69f6439dc585920a5393f491f88b618f
Richard Memory <richardfmemory@churchofjesuschrist.org>
2019-05-27 16:56:28 -0600

    Add backend part of user creation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
+//Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO Use express middlware to populate current user
 
 // This is the entry point for the Yoga server
